eae17a0e109d726156fa458b21a31d2d93d601be
wangzheng95 <863676514@qq.com>
2020-04-29 10:15:16 +0800

    1px问题

---
+++ src/components/swiper/swiper.vue
@@ -0,0 +1,63 @@
+<template>
+  <!-- 轮播图begin -->
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <slot />
+      </div>
+      <!-- 如果需要分页器 -->
+      <div class="swiper-pagination"></div>
+    </div>
+    <!-- 轮播图end -->
+</template>
+
+<script>
+import swiper from 'swiper'
+import 'swiper/css/swiper.css'
+export default {
+  name: 'swiper',
+  mounted () {
+    /* eslint-disable */
+    new swiper(this.$el, {
+      pagination: {
+        el: ".swiper-pagination"
+      },
+      autoplay: this.autoplay ? {
+        delay:this.autoplay,
+        stopOnlastSlide:false,
+        displayOnInteraction: true,
+      } : false,
+      loop: this.loop
+    });
+    /* eslint-ensable */
+  },
+  props: {
+    autoplay: {
+      type: Number,
+      default: 0
+    },
+    loop: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.swiper-container {
+  width: 100%;
+  height: 250px;
+  background: pink;
+  .swiper-pagination-bullet {
+    width: 6px;
+    height: 6px;
+    background: #fff;
+    opacity: 1;
+  }
+  .swiper-pagination-bullet-active {
+    width: 24px;
+    background: #ff7830;
+    border-radius: 6px;
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="page-home">
+    <!-- 首页头部begin -->
     <header class="header">
       <h1 class="logo">
         <a href="/"
@@ -10,26 +11,66 @@
       </h1>
       <a href="/search/"><i class="iconfont icon-sousuo"></i></a>
     </header>
+    <br>
+    <!-- 首页头部end -->
+    <swiper :autoplay='1000' :loop='true' class='my-swiper'>
+        <!-- <div class="swiper-slide">Slide 1</div>
+        <div class="swiper-slide">Slide 2</div>
+        <div class="swiper-slide">Slide 3</div> -->
+        <swiperItem>
+          <a href="">
+            <img src="../../assets/banner/01-banner.jpg" alt="">
+          </a>
+        </swiperItem>
+         <swiperItem>
+          <a href="">
+            <img src="../../assets/banner/02-banner.jpg" alt="">
+          </a>
+        </swiperItem>
+         <swiperItem>
+          <a href="">
+            <img src="../../assets/banner/03-banner.jpg" alt="">
+          </a>
+        </swiperItem>
+         <swiperItem>
+          <a href="">
+            <img src="../../assets/banner/04-banner.jpg" alt="">
+          </a>
+        </swiperItem>
+         <swiperItem>
+          <a href="">
+            <img src="../../assets/banner/05-banner.jpg" alt="">
+          </a>
+        </swiperItem>
+    </swiper>
   </div>
 </template>
 
 <script>
+// import swiper from '@/components/swiper/swiper.vue'
+// import swiperItem from '@/components/swiper/swiperItem.vue'
+import { swiper, swiperItem } from '@/components/swiper'
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    swiper,
+    swiperItem
+  }
 }
 </script>
 <style lang="scss" scoped>
+@import '../../assets/styles/mixins.scss';
 .page-home {
   height: 100%;
   // width: 100%;
   // display: flex;
   // flex-direction: column;
   .header {
-    position: fixed;
-    top: 0;
-    width: 100%;
+    @include border-bottom;
+    display: flex;
+    justify-content: space-between;
     height: 44px;
-    border-bottom: 1px solid #ebebeb;
+    // border-bottom: 1px solid #ebebeb;
     text-align: center;
     line-height: 44px;
     font-size: 36px;
@@ -47,13 +88,17 @@ export default {
       }
     }
     .iconfont {
-      display: block;
+      // display: block;
       float: right;
       margin-right: 12px;
       font-size: 44px;
       color: #ff7830;
-      font-size:26px;
+      font-size: 26px;
     }
+  }
+  .my-swiper img{
+    width: 100%;
+    height: 100%;
   }
 }
 </style>
